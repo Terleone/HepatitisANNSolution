2f39bec2ed30fea2f3e067a38263f5e485e10743
eldritch-rudder <eldritch.rudder@gmail.com>
2019-03-29 15:13:38 +0100

    Added making ranking of features and scipy

--- HepatitisANNSolution/runners/feature_ranker.py
+++ HepatitisANNSolution/runners/feature_ranker.py
@@ -10,6 +10,7 @@ samples = reader.read(data_file_name)
 presenter = SamplePresenter()
 presenter.print(samples)
 
+ranking = []
 """Creating an empty Sample object to iterate over names of it's attributes."""
 for feature_name, none in Sample('', '', '', '', '', '', '', '', '', '',
                                  '', '', '', '', '', '', '', '', '', '', ).__dict__.items():
@@ -23,6 +24,14 @@ for feature_name, none in Sample('', '', '', '', '', '', '', '', '', '',
         elif sample.classification == '2':
             live.append(getattr(sample, feature_name))
     result = stats.ks_2samp(die, live)
+    if result[1] <= 0.1:
+        ranking.append((feature_name, result[0]))
+
     print('Wynik dla cechy ' + feature_name + ':\n\t'
           + 'statistics = ' + str(result[0]) + '\n\t'
           + 'pvalue = ' + str(result[1]) + '\n')
+
+ranking.sort(key=lambda tup: tup[1], reverse=True)
+print("Cechy wedlug przydatnosci:")
+for feature in ranking:
+    print("\t" + feature[0])
